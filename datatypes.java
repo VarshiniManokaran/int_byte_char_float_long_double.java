441635c8bf38694ceb2635cb3e1a2c1b3bb4db8b
VarshiniManokaran <varshini6678@gmail.com>
2024-08-07 13:17:15 +0530

    Add files via upload

---
+++ datatypes.java
@@ -0,0 +1,30 @@
+public class datatypes {
+    public static void main(String[] args) {
+        
+        int intValue=100;
+        System.out.println("intValue is:"+intValue);
+
+
+        byte byteValue=67;
+        System.out.println("Byte Value is:"+byteValue);
+
+
+
+        float floatValue = 58.999f;
+        System.out.println("Float Value is:"+floatValue);
+
+
+        char charValue= 'v';
+        System.out.println("Char Value is:"+charValue);
+
+
+        double doubleValue=55.87;
+        System.out.println("Double Value is:"+doubleValue);
+
+
+
+        long longValue = 2463549876545667786l;
+        System.out.println("Long Value is:"+longValue);
+    }
+    
+}
